2ce976d18f3989d88565df7a922b6f5a410802fd
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-05 14:56:46 -0800

    ll-merge

--- linked-list.js
+++ linked-list.js
@@ -15,8 +15,8 @@ class LinkedList {
     }
     includes(val) {
         let current = this.head;
-        while(current !== null) {
-            if(current.value === val) {
+        while (current !== null) {
+            if (current.value === val) {
                 return true;
             }
             current = current.next;
@@ -26,7 +26,7 @@ class LinkedList {
     toString() {
         let str = '';
         let current = this.head;
-        while(current !== null) {
+        while (current !== null) {
             str += `${current.value} `;
             current = current.next;
         }
@@ -37,36 +37,36 @@ class LinkedList {
 
         let node = new Node(newVal); 
         let current = this.head ; 
-        while(current.next.value !== val) {
-                current = current.next
-            }
-            node.next = current.next; 
-            current.next = node;
+        while (current.next.value !== val) {
+            current = current.next;
         }
+        node.next = current.next; 
+        current.next = node;
+    }
     
 
     insertAfter(val, newVal) {
 
         let node = new Node(newVal);
-        let curent = this.head; 
-        while(current.value !== val) {
-                current.next = next;
+        let current = this.head; 
+        while (current.value !== val) {
+            current.next = next;
         } 
         node.next = current.next;
         current.next = node;
-      }
+    }
     
 
     append(value){
         const node = new Node(value);
         let current = this.head;
-        while(current.next !== null){
-          current = current.next;
+        while (current.next !== null){
+            current = current.next;
         }
         current.next = node;
-      }
+    }
 
     
-};
+}
 
 module.exports = LinkedList;
