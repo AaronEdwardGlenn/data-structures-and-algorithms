920dd5b67c460ca016ab251ad6c529bf6c4c2ae6
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-23 15:57:05 -0800

    added node test

--- tree/tree.test.js
+++ tree/tree.test.js
@@ -1,46 +1,12 @@
-class Node {
-  constructor(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
 
-class BinarySearchTree {
-  constructor(root = null){
-    this.root = new Node(root);
-  }
+const { Node, BinarySearchTree } = require('../challenges/tree/tree');
 
-  add(value){
-    let node = new Node(value);
-    if(!this.root) this.root = node;
-    else {
-      let current = this.root;
-      // eslint-disable-next-line no-extra-boolean-cast
-      while(!!current){
-        if(node.value < current.value){
-          if(!current.left){
-            current.left = node;
-            break;
-          }
-          current = current.left;
-        }
-        if(node.value >= current.value){
-          if(!current.right){
-            current.right = node;
-            break;
-          }
-          current = current.right;
-        }
-        else {
-          break;
-        }
-      }
-    }
-    return this;
-  }
+describe('Node class', () => {
+  let node;
+  beforeEach(() => {
+    node = new Node(3);
+  });
 
-}
-
-module.exports = { Node, BinarySearchTree };
-
+  it('has a value', () => {
+    expect(node.value).toEqual(3);
+  });
